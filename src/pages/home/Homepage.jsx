b30e19da2344347f554adfd1146efb7e07271ac4
soykot2910 <ashrafulislam2910@gmail.com>
2021-09-27 12:45:47 +0600

    menu-item directory component done

---
+++ src/pages/home/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DirectoryComp from "../../components/common/DirectoryComp/DirectoryComp";
+import "./homepage.style.scss";
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to my Homepage</h1>
+      <DirectoryComp />
+    </div>
+  );
+};
+
+export default Home;
